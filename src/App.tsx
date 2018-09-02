c07c1bc6cd4ebe9725ca832e4f8ff4d9133a1a99
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-02 11:15:49 +0530

    init: models

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,30 @@ import {Component} from 'react'
 import './App.css'
 import logo from './logo.svg'
 
+
+function Task({ id, title, done, createdAt, category}:{id:string,title:string,done:boolean,createdAt:number,category:Category}) {
+  return { id, title, done, createdAt, category }
+}
+
+enum Category {
+  InBasket,
+  NextAction,
+  Project,
+  SomeDay,
+}
+export function createRandomTask() {
+  const defaults = {
+    category: faker.random.arrayElement([Category.InBasket]),
+    createdAt: Date.now(),
+    done: faker.random.boolean(),
+    id: faker.random.alphaNumeric(3),
+    title: faker.random.word(),
+  }
+  return Task(defaults)
+}
+
 const task = {
   id: faker.random.alphaNumeric(3),
-
 }
 
 class App extends Component {
@@ -17,7 +38,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <div>task.id={task.id}</div>
+        <div>task.id={task.id + 1}</div>
       </div>
     )
   }
